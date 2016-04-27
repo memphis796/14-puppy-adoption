bdf6b3f335603ae710de38251aa5df8936e6a256
Memphis796 <memphis796@Hotmail.com>
2016-04-26 19:48:09 -0500

    adding puppies

--- src/application-view.js
+++ src/application-view.js
@@ -5,26 +5,18 @@ import PuppyView from 'puppy-view';
 export default class ApplicationView {
   constructor(element) {
     this.nav = element.querySelector(`.top-nav`);
-    this.list = element.querySelector(`.puppy-list`);
+    this.info = element.querySelector(`.input__info`);
+    this.puppyList = element.querySelector(`.puppy-list`);
 
-    const create = new CreateFormView(this.nav);
-
+    this.create =
     fetch(`http://tiny-tn.herokuapp.com/collections/ryan-puppy`)
     .then((res) => res.json())
     .then((data) => {
-      this.data = data;
+      data.forEach((item) => {
+        const view = new PuppyView(item, this);
 
-      this.render();
+        this.puppyList.appendChild(view.card);
+      });
     });
-  }
-
-  render() {
-    this.list.innerHTML = ``;
-    this.data.forEach((puppy) => {
-      const puppyCard = new PuppyView(puppy);
-
-      this.list.appendChild(puppyCard.card);
-    });
-
   }
 }
